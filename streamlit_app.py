e09042ca034b9d56415647a08e425fd396555fac
K Ihe <zkwbi@outlook.com>
2025-08-21 20:27:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,16 @@
-import streamlit as st
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 
-st.title("✅ Hello Streamlit App")
-st.write("If you're seeing this, your app is running correctly from GitHub!")
+import random
+import networkx as nx
+import matplotlib.pyplot as plt
+from textblob import TextBlob
+import streamlit as st
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, classification_report
+import numpy as np
+import feedparser
+import matplotlib.colors as mcolors
+import matplotlib.patches as mpatches
