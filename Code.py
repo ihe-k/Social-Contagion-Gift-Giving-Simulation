990fbe612f7b0342ac9a3d3f66e499a50235a65c
K Ihe <226985594+ihe-k@users.noreply.github.com>
2025-09-01 01:37:57 +0100

    Update Code.py

--- Code.py
+++ Code.py
@@ -5,7 +5,6 @@ from textblob import TextBlob
 import streamlit as st
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import matplotlib.patches as mpatches
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
